745324a8b9fae8d3701ad3c419e8414c7146b60c
John O'Connor <jpo424@gmail.com>
2016-11-16 11:52:44 -0500

    moving to release versioning scheme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pycharts',
-    version='0.1.1b1',
+    version='0.1.1',
     url='https://github.com/ycharts/pycharts',
     license='MIT',
     description='Client for the YCharts API',
