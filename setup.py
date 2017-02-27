bfd4c82be694c031586d2de9e32e54df2fca35dd
KFoxder <kevin_fox@me.com>
2017-02-27 15:16:08 -0500

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pycharts',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/ycharts/pycharts',
     license='MIT',
     description='Client for the YCharts API',
